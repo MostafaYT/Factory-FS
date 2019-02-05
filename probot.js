c4961f0fa97620270b2f1e4bb5e9cd5af29355d2
MostafaYT <44404636+MostafaYT@users.noreply.github.com>
2019-02-05 17:38:35 +0200

    Update probot.js

--- probot.js
+++ probot.js
@@ -835,6 +835,23 @@ client.on("message", (message) => {
     message.channel.send(`Hoold on!`).then(m => {
     m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
     });
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.toLowerCase().startsWith(prefix + `help`)) {
+    const embed = new Discord.RichEmbed()
+    .setTitle(`:mailbox_with_mail: Vulnix Help`)
+    .setColor(0xCF40FA)
+    .setDescription(`Hello! I'm Vulnix, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
+    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
+    .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about Vulnix`)
+    message.channel.send({ embed: embed });
+  }
+
+  if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
+    message.channel.send(`Hoold on!`).then(m => {
+    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+    });
 }
 
 if (message.content.toLowerCase().startsWith(prefix + `new`)) {
@@ -864,7 +881,7 @@ if (message.content.toLowerCase().startsWith(prefix + `new`)) {
         c.send({ embed: embed });
     }).catch(console.error);
 }
-if (message.content.toLowerCase().startsWith(prefix + `delete`)) {
+if (message.content.toLowerCase().startsWith(prefix + `close`)) {
     if (!message.channel.name.startsWith(`ticket_`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
 
     message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`_confirm\`. This will time out in 10 seconds and be cancelled.`)
@@ -878,7 +895,7 @@ if (message.content.toLowerCase().startsWith(prefix + `delete`)) {
           message.channel.delete();
         })
         .catch(() => {
-                 m.edit('Ticket delete timed out, the ticket was not deleted.').then(m2 => {
+          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
               m2.delete();
           }, 3000);
         });
