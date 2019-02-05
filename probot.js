3f2202bbac136cce11fa081c1362a724fc47c698
MostafaYT <44404636+MostafaYT@users.noreply.github.com>
2019-02-05 10:07:03 +0200

    Update probot.js

--- probot.js
+++ probot.js
@@ -783,4 +783,16 @@ message.channel.stopTyping()
 })
 }
 });
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`$help`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
 client.login(process.env.BOT_TOKEN);
