3d8399ec917f2db0044fe8b7bf2ae7945a831b7f
MostafaYT <44404636+MostafaYT@users.noreply.github.com>
2019-02-05 09:53:41 +0200

    Update probot.js

--- probot.js
+++ probot.js
@@ -3,7 +3,6 @@ const moment = require("moment");
 const fs = require("fs");      
 const dateFormat = require('dateformat');
 const client = new Discord.Client(); 
-const Canvas = require("canvas"); //npm i canvas
 const prefix = "YOUR Prefix Here"
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 
